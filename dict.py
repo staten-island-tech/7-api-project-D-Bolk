98d153dbcacd343f17b9c4c9b2cbf70ce1dbf38d
D-Bolk <danielb643@nycstudents.net>
2026-03-30 09:26:07 -0400

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,19 @@
+sushi_orders = [
+    {"name": "California Roll", "price": 8},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8},
+    {"name": "Dragon Roll", "price": 12},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Miso Soup", "price": 4},
+    {"name": "Edamame", "price": 5},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8}
+]
+
+def receipts(orders):
+    receipt = {}
+    for order in orders:
+        if order['name'] in receipt:
+         print('name', 'price')
+print(sushi_orders)
